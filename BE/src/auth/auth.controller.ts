f0db186423482651598e1cf4d17f577cdd6dccdd
abhishekawari3 <abhishekawari830@gmail.com>
2026-03-04 18:11:29 +0530

    feat(auth): refactor signup functionality and improve error handling

--- BE/src/auth/auth.controller.ts
+++ BE/src/auth/auth.controller.ts
@@ -1,31 +1,28 @@
 import type { Request, Response, NextFunction } from 'express';
 import type { SignupDTO } from './auth.types.js';
-
+import { signupController } from './auth.services.js';
 
 
 export const signup = async (
     req: Request,
-    res:Response,
-    next:NextFunction
+    res: Response,
+    next: NextFunction
 ) => {
     try{
         const {name,email,password}: SignupDTO = req.body;
-
-        const existing_User: any = await User.findOne({email});
-
-        if(existing_User) {
-            return res.status(400).json({
-                success: false,
-                message: 'email already exists'
-            });
-        }
-
-        const hashpassword : string = await bcrypt.hash(password,10);
-
-        User.create({
-            name: name,
-            email: email,
-            password: hashpassword
-        } );
+        const data: any = await signupController(req,res,next);
+        
+         res.status(201).json({
+            success: true,
+            message: 'User created successfully',
+            data: data
+        });
+        
+    } catch(err: any){
+        res.status(500).json({
+            success: false,
+            message: 'Internal server error',
+            error: err.message
+        });
     }
 }
--- BE/src/auth/auth.services.ts
+++ BE/src/auth/auth.services.ts
@@ -1,10 +1,55 @@
 import jwt from 'jsonwebtoken';
 import { User } from './auth.model.js';
 import bcrypt from 'bcrypt';
-import type { NextFunction } from 'express';
+import type { Request,Response, NextFunction } from 'express';
 
-export const signup = async (
+export const signupController = async (
     req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    const { email, password, name } = req.body;
+    const existing_User: any = await User.findOne({email});
+
+        if(existing_User) {
+            return res.status(400).json({
+                success: false,
+                message: 'email already exists'
+            });
+        }
+
+        const hashpassword : string = await bcrypt.hash(password,10);
+
+        User.create({
+            name: name,
+            email: email,
+            password: hashpassword
+        });
+};
+
+export const loginController = async(
+    req:Request,
     res:Response,
     next: NextFunction
-)
+) =>{
+    const  { email, password } = req.body;
+
+    const user: any = await User.findOne({email});
+
+    if(!user){
+        return res.status(400).json({
+            success: false,
+            message: 'Invalid credentials'
+        });
+    }
+
+    const isMatch : boolean = await bcrypt.compare(password, user.password);
+
+    if(!isMatch){
+        return res.status(400).json({
+            success: false,
+            message: 'Invalid credentials'
+        })
+    }
+
+}
