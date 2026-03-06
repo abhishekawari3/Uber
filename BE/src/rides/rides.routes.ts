68eb6df743b9703b0d0175ee84cafbabe23f7088
abhishekawari3 <abhishekawari830@gmail.com>
2026-03-06 22:31:12 +0530

    feat(rides): add initial rides route with POST endpoint

---
+++ BE/src/rides/rides.routes.ts
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+import type { NextFunction, Request, Response } from 'express';
+
+const router: Router = Router();
+
+router.post('/rides', (req: Request, res: Response, next: NextFunction) => {
+  
+});
