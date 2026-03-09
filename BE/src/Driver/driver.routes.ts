53cbd4dbc865bd1a864b1bd39522957a2ebd154d
abhishekawari3 <abhishekawari830@gmail.com>
2026-03-09 20:54:26 +0530

    Routes

--- BE/src/Driver/driver.routes.ts
+++ BE/src/Driver/driver.routes.ts
@@ -1,4 +1,12 @@
 import { Router } from 'express';
 import type { Request, Response, NextFunction } from 'express';
 
-const router: Router = router();
+const router: Router = Router();
+
+router.post('/status',(
+    req: Request,
+    res:Response,
+    next:NextFunction
+)=>{
+    const isAvailbility 
+})
